025d75a0b92f63f0444f50dce9f9d04a6ad1cd9f
seul-dev <seul.dev@gmail.com>
2022-10-18 23:34:27 +0900

    feat: not found page - navigate 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/common/Button';
 
 const NotFound = () => {
-  return <div>Page not found!</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>Page not found!</div>
+      <Button onClick={() => navigate(-1)} type='button' disabled={false}>
+        돌아가기
+      </Button>
+    </>
+  );
   //back to home 버튼
 };
 
